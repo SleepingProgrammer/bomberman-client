1928e96e24ff5cdad59a344bc154c404a412526e
SleepingProgrammer <32633305+SleepingProgrammer@users.noreply.github.com>
2023-08-05 17:02:20 +0800

    Test auto deploy

--- src/Constants.js
+++ src/Constants.js
@@ -4,6 +4,7 @@ const buildMode = import.meta.env.VITE_BUILD_ENVIRONMENT;
 
 //LOCAL is default
 Constants.COLYSEUS_SERVER = "ws://localhost:8080";
+Constants.COLYSEUS_SERVER = "ws://192.81.132.252:8080";
 Constants.ROOM_NAME = "bomberman_room";
 
 if (buildMode === "STAGE") {
